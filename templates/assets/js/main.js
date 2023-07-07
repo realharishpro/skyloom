ffffcfc3b1da44575cca07f5e0453a9aae5c0bfc
Harish Murugan <85951348+realharishpro@users.noreply.github.com>
2023-07-07 11:26:12 +0530

    Added JS Files

---
+++ templates/assets/js/main.js
@@ -0,0 +1,81 @@
+(function ($) {
+	"use strict";
+
+/*=============================================
+	=    		 Preloader			      =
+=============================================*/
+function preloader() {
+	$('#preloader').delay(0).fadeOut();
+};
+
+$(window).on('load', function () {
+	preloader();
+	mainSlider();
+	wowAnimation();
+});
+
+
+
+
+/*=============================================
+	=     Menu sticky & Scroll to top      =
+=============================================*/
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$("#sticky-header").removeClass("sticky-menu");
+		$('.scroll-to-target').removeClass('open');
+
+	} else {
+		$("#sticky-header").addClass("sticky-menu");
+		$('.scroll-to-target').addClass('open');
+	}
+});
+
+
+/*=============================================
+	=    		 Scroll Up  	         =
+=============================================*/
+if ($('.scroll-to-target').length) {
+  $(".scroll-to-target").on('click', function () {
+    var target = $(this).attr('data-target');
+    // animate
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 1000);
+
+  });
+}
+
+
+
+/*=============================================
+	=          Data Background               =
+=============================================*/
+$("[data-background]").each(function () {
+	$(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+})
+
+
+
+/*=============================================
+	=    	   Toggle Active  	         =
+=============================================*/
+$('.flight-detail-wrap').slideUp();
+$('.detail').on('click', function () {
+	$(this).toggleClass('show');
+	$(this).parent().parent().parent().parent().find('.flight-detail-wrap').slideToggle();
+});
+
+
+/*=============================================
+	=           DatePicker Active             =
+=============================================*/
+$(function () {
+	$(".form-grp .date").datepicker({
+		autoclose: true,
+		todayHighlight: true
+	}).datepicker('update', new Date());
+});
+
+})(jQuery);
